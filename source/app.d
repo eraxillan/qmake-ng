73ee256b53b8b4d7ab2b613fec3508a42b7c1bb5
Alexander Kamyshnikov <axill777@gmail.com>
2017-12-14 09:16:25 +0300

    Various fixes in grammar

--- source/app.d
+++ source/app.d
@@ -1,5 +1,6 @@
 import std.typecons : No;
 
+import std.algorithm;
 import std.conv;
 import std.stdio;
 import std.file;
@@ -60,36 +61,45 @@ static string preprocessLines(string[] strLinesArray)
         }
         
         // FIXME: workaround for grammar ambiguity - cannot distingush AND-colon and scope statement expression end colon
-        // Replace last colon (":") with "@"
+        // Replace last colon (":") with "@";
+        // Exceptions:
+        // - there is an "{" on this line
+        // - (TODO) the colon is inside quotes/doublequotes
         auto lastColonIndex = strLine.lastIndexOf(':');
-        if (lastColonIndex != -1)
+        if ((lastColonIndex != -1) && !strLine.endsWith("{"))
         {
             strLine.replaceInPlace(lastColonIndex, lastColonIndex + 1, "@");
         }
+        // Replace "else:" with "else@"
+        // FIXME: cover all cases!
+        strLine = strLine.replace("else:", "else@");
 
-        if (isMultiLine)
+/*        if (isMultiLine)
             writeln("Multi-line " ~ std.conv.to!string(startLineIndex + 1) ~ " - " ~ std.conv.to!string(endLineIndex + 1) ~ ": |" ~ strLine ~ "|");
         else
-            writeln("Line " ~ std.conv.to!string(lineIndex + 1) ~ ": |" ~ strLine ~ "|");
+            writeln("Line " ~ std.conv.to!string(lineIndex + 1) ~ ": |" ~ strLine ~ "|");*/
 
-        result ~= strLine;
+//        if (!strLine.empty())
+            result ~= strLine;
     }
 
     return result.join("\n");
 }
 
-void main()
+bool tryParseProject(string fileName)
 {
     mixin(grammar(`
     QMakeProject:
         Project <- Statement+ eoi
-        Statement <- Block | Comment | Assignment | FunctionCall | Scope | EmptyStatement
+        Statement <- Block | Comment | Assignment | TestFunctionCall | Scope | EmptyStatement
 
         # No input
-        EmptyStatement <- eps
+        EmptyStatement <- eps eol*
 
         # Code block
-        Block <- :space* "{" :space* :eol+ :space* (Statement)+ :space* "}" :space* :eol*
+        Block           <- SingleLineBlock / MultiLineBlock
+        SingleLineBlock <- :space* "@" :space* Statement
+        MultiLineBlock  <- :space* "{" :space* :eol+ :space* (Statement)* :space* :eol* "}" :space* :eol*
 
         # Comment, single-line and multi-line (extension)
         Comment                         <~ MultiLineComment / SingleLineComment
@@ -98,44 +108,66 @@ void main()
         SingleLineCommentChars          <- SingleLineCommentChar+
         SingleLineCommentChar           <- !LineTerminator SourceCharacter
 
-        # Variable assignment
-        Assignment <- (DirectAssignment / AppendAssignment / AppendUniqueAssignment / RemoveAssignment / ReplaceAssignment) eol?
-        DirectAssignment        < Identifier "=" Expression
-        AppendAssignment        < Identifier "+=" Expression
-        AppendUniqueAssignment  < Identifier "*=" Expression
-        RemoveAssignment        < Identifier "-=" Expression
-        ReplaceAssignment       < Identifier "~=" Expression
+        # Variable or variable property assignment
+        Assignment <- (DirectAssignment / AppendAssignment / AppendUniqueAssignment / RemoveAssignment / ReplaceAssignment)
+ 
+        StringList              <- String (:space* String)*
+        String                  <- StringChars?
+        StringChars             <- ~(StringChar+)
+        #StringChar              <- !(blank / LineTerminator / "#" / "," / "(" / ")" / quote / doublequote ) SourceCharacter
+        StringChar              <- !(blank / LineTerminator) SourceCharacter
+        
+        DirectAssignment        <- QualifiedIdentifier :space* "="  :space* StringList? :eol*
+        AppendAssignment        <- QualifiedIdentifier :space* "+=" :space* StringList? :eol*
+        AppendUniqueAssignment  <- QualifiedIdentifier :space* "*=" :space* StringList? :eol*
+        RemoveAssignment        <- QualifiedIdentifier :space* "-=" :space* StringList? :eol*
+        ReplaceAssignment       <- QualifiedIdentifier :space* "~=" :space* StringList? :eol*
 
         # Test function call
         # E.g.:
         # message("Starting project build...")
-        FunctionCall < Identifier "(" FunctionArgumentList? ")"
-        FunctionArgumentList < FunctionArgumentString (:"," FunctionArgumentString)*
-        FunctionArgumentString <- EnquotedString | RegularFunctionArgumentString
+        TestFunctionCall < Identifier "(" FunctionArgumentList? ")"
+        FunctionArgumentList < FunctionArgumentString ((:"," FunctionArgumentString)* | (:blank* FunctionArgumentString)*)
+        FunctionArgumentString <- EnquotedString | RegularFunctionArgumentString | ReplaceFunctionCall | TestFunctionCall
         RegularFunctionArgumentString <- ~(RegularFunctionArgumentStringChar+)
         RegularFunctionArgumentStringChar <- !(blank / "," / ")" / quote / doublequote ) SourceCharacter
+
+# FIXME: function arguments can contain "("/")" themselves, so we need special rule to detect function argument list end
+        EndOfFunction <- ")" :space* (eol* | "@" | "{" | ":" | "|")
+
+        # Replace function call (must be rvalue only)
+        # E.g.:
+        # $$escape_expand("One\nTwo\nThree")
+        ReplaceFunctionCall < "$$" Identifier "(" FunctionArgumentList? ")"
         
         # Conditional statement
         # E.g.:
         # CONFIG(debug, debug|release):buildmode = debug
-        Scope       < BooleanExpression ("@" Statement | Block) ElsePart*
-        ElsePart    < "else@" Statement | "else" Block
+        Scope            <- BooleanExpression ScopeTrueBranch ScopeFalseBranch*
+        ScopeTrueBranch  <- Block
+        ScopeFalseBranch <- "else" :space* Block
+#        ScopeFalseBranch <- ("else" :space* "@" :space* Statement) | ("else" :space* Block)
 
         # E.g.:
         # var1: message("scope 1.1")
         # !exists($$QMAKE_QT_CONFIG)|!include($$QMAKE_QT_CONFIG, "", true) {
-        BooleanExpression           <- LogicalORExpression
-        LogicalORExpression         <- LogicalANDExpression (:space* "|" :space* LogicalANDExpression)*
-        LogicalANDExpression        <- LogicalNOTExpression (:space* ":" :space* LogicalNOTExpression)*
-        LogicalNOTExpression        <- (:space* "!" :space*)? PrimaryBooleanExpression
-        PrimaryBooleanExpression    <- '(' BooleanExpression ')'
-                                     / BooleanAtom
+        BooleanExpression            <- LogicalORExpression
+        LogicalORExpression          <- LogicalANDExpression (:space* "|" :space* LogicalANDExpression)*
+        LogicalANDExpression         <- LogicalNOTExpression (:space* ":" :space* LogicalNOTExpression)*
+        LogicalNOTExpression         <- (:space* "!" :space*)? PrimaryBooleanExpression
+        PrimaryBooleanExpression     <- ParenthesedBooleanExpression
+                                      / IfTestFunctionCall
+                                      / BooleanAtom
+        ParenthesedBooleanExpression <- '(' BooleanExpression ')'
+# FIXME: support CONFIG test function
+        IfTestFunctionCall           <- "if" :space* "(" :space* BooleanExpression :space* ")"
+        BooleanAtom                  <- Identifier | ExpandStatement | TestFunctionCall | BooleanConst
+        BooleanConst                 <- "true" | "false"
 
-        BooleanAtom                 <- Identifier | FunctionCall | BooleanConst
-        BooleanConst                <- "true" | "false"
 
-
-        Expression <- RawString
+        Expression <- EmptyStatement | RawString
+        
+        ExpandStatement <- "$$" :space* QualifiedIdentifier / "$$" :space* "{" :space* QualifiedIdentifier :space* "}"
 
         # Raw string: can contain any character except of EOL
         RawString       <- RawStringChars?
@@ -155,25 +187,69 @@ void main()
         NonSingleQuoteCharacter   <- !quote .
 
         Identifier <- identifier
+        # Identifier  <~ [a-zA-Z_] [a-zA-Z_0-9]*
+        QMakeIdentifier <~ [a-zA-Z_0-9]*
+        QualifiedIdentifier <~ (Identifier / ExpandStatement) ('.' (QMakeIdentifier / ExpandStatement))*
 
         SourceCharacter <- [\u0000-\uFFFC]
         LineTerminator  <- "\u000A" / "\u000D" / "\u2028" / "\u2029"
     `));
+    
+    auto proFileContents = std.file.readText(fileName);
+    proFileContents = preprocessLines(splitLines(proFileContents));
+        
+    auto parseTree = QMakeProject(proFileContents);
+    if (!parseTree.successful)
+    {
+        writeln("Parsing failed:");
+        writeln(parseTree);
+    }
+    return parseTree.successful;
+}
 
-    // FIXME: add readFromFile function
+unittest
+{
+    assert(tryParseProject("tests/linux-clang-qmake.conf"));
+}
+
+void main()
+{
+/*    if (!tryParseProject("tests/android-base-head.conf"))
+        return;
+    writeln("Test tests/android-base-head.conf passed\n\n");
+    if (!tryParseProject("tests/scope_1.pro"))
+        return;
+    writeln("Test tests/scope_1.pro passed\n\n");
+    if (!tryParseProject("tests/scope_2.pro"))
+        return;
+    writeln("Test tests/scope_2.pro passed\n\n");
+    if (!tryParseProject("tests/scope_3.pro"))
+        return;
+    writeln("Test tests/scope_3.pro passed\n\n");
+    if (!tryParseProject("tests/qml_module.prf"))
+        return;
+    writeln("Test tests/qml_module.prf passed\n\n"); */
 
     // Runtime parsing
-    writeln("Reading project file...");
-    auto proFileContents = std.file.readText("/home/eraxillan/Qt/5.9.1/gcc_64/mkspecs/features/qt_config.prf");
-//    auto proFileContents = std.file.readText("/home/eraxillan/Projects/bankirureader_cmake/rubankireader.pro");
-    proFileContents = preprocessLines(splitLines(proFileContents));
-    
-    auto parseTree = QMakeProject(proFileContents);
-    if (parseTree.successful)
-        writeln("Parse tree '" ~ parseTree.name ~ "':\n");
-    else
-        writeln("Project parsing failed: ");
+    // Iterate over all *.d files in current directory ("") and all its subdirectories
+    auto projectFiles = dirEntries("/home/eraxillan/Qt/5.9.1/gcc_64/mkspecs", SpanMode.depth).filter!(
+        f => f.name.endsWith(".pro") || f.name.endsWith(".pri") || f.name.endsWith(".prf") || f.name.endsWith(".conf")
+    );
+    int successfulCount = 0, failedCount = 0;
+    foreach (d; projectFiles)
+    {
+        writeln("\n");
+        writeln(d.name);
 
-    writeln(parseTree);
+        if (tryParseProject(d.name))
+        {
+            successfulCount++;
+            writeln("SUCCESS");
+        } else failedCount++;
+    }
+    
+    int totalCount = successfulCount + failedCount;
+    writeln("Successfully parsed: " ~ std.conv.to!string(successfulCount));
+    writeln("Failed to parse: " ~ std.conv.to!string(failedCount));
 }
 
